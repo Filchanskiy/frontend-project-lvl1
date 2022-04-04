2d011496c47e403df8c63148ea2fb8e4303bbae5
Pavel <playwikki97@gmail.com>
2022-04-04 15:08:39 +0300

    Simplified invocation of the game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import userName from '../src/cli.js';
+import randomNumber from '../src/games/even.js';
 
-const randomNumber = () => {
-  let counterOfAnswer = 1;
-  while (counterOfAnswer <= 3) {
-    const number = Math.floor(Math.random() * 100);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    const wrongAnswer1 = `\n${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`;
-    const wrongAnswer2 = `\n${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`;
-
-    if (number % 2 === 0 && answer === 'yes' && counterOfAnswer <= 3) {
-      console.log('Correct!');
-      counterOfAnswer += 1;
-    } else if (number % 2 !== 0 && answer === 'no' && counterOfAnswer <= 3) {
-      console.log('Correct!');
-      counterOfAnswer += 1;
-    } else if (number % 2 === 0 && answer !== 'yes') {
-      console.log(wrongAnswer2);
-      counterOfAnswer += 4;
-    } else if (number % 2 !== 0 && answer !== 'no') {
-      console.log(wrongAnswer1);
-      counterOfAnswer += 4;
-    }
-  }
-  if (counterOfAnswer === 4) {
-    console.log(`Congratulation ${userName}!`);
-  }
-};
-
-console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
 randomNumber();
