ae2b350282b0041b28807a2c87c9dc7e55fa773c
Pavel <playwikki97@gmail.com>
2022-04-04 15:10:31 +0300

    1

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-console.log('\nWelcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-
-export default userName;
