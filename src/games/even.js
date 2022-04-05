b8dc996909d1b5e60482b1936dbe67d08d382595
Pavel <playwikki97@gmail.com>
2022-04-05 13:50:17 +0300

    Fixed file v1.1

--- src/games/even.js
+++ src/games/even.js
@@ -6,29 +6,29 @@ console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const randomNumber = () => {
-  let counterOfAnswer = 1;
-  while (counterOfAnswer <= 3) {
+  let counterOfAnswers = 1;
+  while (counterOfAnswers <= 3) {
     const number = Math.floor(Math.random() * 100);
     console.log(`Question: ${number}`);
     const answer = readlineSync.question('Your answer: ');
     const wrongAnswer1 = `\n${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`;
     const wrongAnswer2 = `\n${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`;
 
-    if (number % 2 === 0 && answer === 'yes' && counterOfAnswer <= 3) {
+    if (number % 2 === 0 && answer === 'yes' && counterOfAnswers <= 3) {
       console.log('Correct!');
-      counterOfAnswer += 1;
-    } else if (number % 2 !== 0 && answer === 'no' && counterOfAnswer <= 3) {
+      counterOfAnswers += 1;
+    } else if (number % 2 !== 0 && answer === 'no' && counterOfAnswers <= 3) {
       console.log('Correct!');
-      counterOfAnswer += 1;
+      counterOfAnswers += 1;
     } else if (number % 2 === 0 && answer !== 'yes') {
       console.log(wrongAnswer2);
-      counterOfAnswer += 4;
+      counterOfAnswers += 4;
     } else if (number % 2 !== 0 && answer !== 'no') {
       console.log(wrongAnswer1);
-      counterOfAnswer += 4;
+      counterOfAnswers += 4;
     }
   }
-  if (counterOfAnswer === 4) {
+  if (counterOfAnswers === 4) {
     console.log(`Congratulation ${userName}!`);
   }
 };
