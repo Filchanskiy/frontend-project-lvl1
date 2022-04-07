f437580ead0dd5a711bf95aece17f3d4275a0129
Pavel <playwikki97@gmail.com>
2022-04-07 18:31:21 +0300

    Added const right

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const startGames = () => {
+export const startGames = () => {
   console.log('\nWelcome to the Brain Games!');
   const askName = () => readlineSync.question('May I have your name? ');
   const userName = askName();
   return userName;
 };
-export default startGames;
+
+export const right = 'Correct!';
