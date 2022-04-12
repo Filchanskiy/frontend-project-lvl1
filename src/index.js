a20df4b9e43479c7e7eb690d7e2382488b12b545
Pavel <playwikki97@gmail.com>
2022-04-12 13:13:10 +0300

    Added several exported functions, also fixed the greeting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ export const startGames = () => {
   console.log('\nWelcome to the Brain Games!');
   const askName = () => readlineSync.question('May I have your name? ');
   const userName = askName();
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
-
 export const right = 'Correct!';
+
+export const askAnswer = () => {
+  const answer = readlineSync.question('Your answer: ');
+  return answer;
+};
+
+export const firstQuestion = () => console.log('What is the result of the expression?'); // Вопрос к первому заданию.
