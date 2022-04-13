df4048e05de4443d06683185fd02362a8db41a2f
Pavel <playwikki97@gmail.com>
2022-04-13 12:24:55 +0300

    Moved all game architecture to index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import readlineSync from 'readline-sync';
 
-export const startGames = () => {
+export const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const startGames = (task, game) => {
   console.log('\nWelcome to the Brain Games!');
   const askName = () => readlineSync.question('May I have your name? ');
   const userName = askName();
   console.log(`Hello, ${userName}!`);
-  return userName;
+  console.log(task);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = game();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'. \nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}`);
 };
-
-export const right = () => console.log('Correct!');
-
-export const askAnswer = () => {
-  const answer = readlineSync.question('Your answer: ');
-  return answer;
-};
-
-export const congratulations = (name) => console.log(`Congratulations, ${name}!`);
-
-export const firstTask = () => console.log('Answer "yes" if the number is even, otherwise answer "no".'); // Задание к первой игре.
-export const secondTask = () => console.log('What is the result of the expression?'); // Задание ко второй игре.
-export const thirdTask = () => console.log('Find the greatest common divisor of given numbers.'); // Задание к третьей игре.
-export const fourthTask = () => console.log('What number is missing in the progression?'); // Задание к четвертой игре.
-export const fifthTask = () => console.log('Answer "yes" if given number is prime. Otherwise answer "no".'); // Задание к пятой игре.
